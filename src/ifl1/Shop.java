3ed77ca2fdf47f5ae7db94e44acc9ec3a0837b40
Levin <levin-janssen@web.de>
2022-12-16 19:17:03 +0100

    Shop Class

--- src/ifl1/Shop.java
+++ src/ifl1/Shop.java
@@ -1,4 +1,22 @@
 package ifl1;
 
+import java.util.Queue;
+
 public class Shop {
+    private String name;
+    private double balance;
+    private Queue<Customer> customerQueue;
+
+    public Shop(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
