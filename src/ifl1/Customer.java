08d9d547f3612b6e249320de89bfd2533bbab4f0
Levin <levin-janssen@web.de>
2022-12-16 19:09:36 +0100

    customer and product class

--- src/ifl1/Customer.java
+++ src/ifl1/Customer.java
@@ -1,4 +1,19 @@
 package ifl1;
 
 public class Customer {
+    private String name;
+    private double money;
+
+    public Customer(String name, double money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
 }
